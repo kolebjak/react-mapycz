aa205b1bbcc987d85f92e1be095f5af4da5ada20
Michal Kacerovský <kacerovsky.michal@gmail.com>
2017-10-04 22:07:22 +0200

    Playground update

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import Map from 'react-mapycz';
+import Map, {ControlPane, ZoomControl, CompassControl} from 'react-mapycz';
 
 const App = () => (
 	<div>
 		<h1>Hi</h1>
+		<Map>
+			<ControlPane>
+				<ZoomControl/>
+			</ControlPane>
+		</Map>
+		<Map>
+			<ControlPane>
+				<CompassControl left="50"/>
+			</ControlPane>
+		</Map>
 		<Map/>
 	</div>
 );
