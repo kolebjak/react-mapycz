52f5bd576885826785f8bdee0ff32c1660411090
CrossTheDev <54584277+CrossTheDev@users.noreply.github.com>
2021-12-02 00:52:37 +0100

    385 Add MouseControl props update, add disabled prop (#387)
    * Update MouseControl on props change (#385)
    
    * Add disabled prop to MouseControl (#385)
    
    * Reformat MouseControl (#385)
    
    Co-authored-by: Michaela Krížová <michaela.krizova@prorocketeers.com>

--- src/controls/MouseControl.tsx
+++ src/controls/MouseControl.tsx
@@ -1,36 +1,53 @@
-import {useContext, useEffect} from 'react';
-import {MapContext} from "../Map";
+import {useContext, useEffect, useRef} from 'react';
+import {MapContext} from '../Map';
 
 interface MouseControlProps {
   pan?: boolean;
   wheel?: boolean;
   zoom?: boolean;
+  disabled?: boolean;
 }
 
 const MouseControl = (props: MouseControlProps) => {
-  const {pan, wheel, zoom} = props;
-  let mode = 0;
+  const {pan, wheel, zoom, disabled} = props;
 
-  mode |= pan && window.SMap.MOUSE_PAN;
-  mode |= zoom && window.SMap.MOUSE_ZOOM;
-  mode |= wheel && window.SMap.MOUSE_WHEEL;
-
-  const map = useContext<any>(MapContext)
-  const mouseControl = new window.SMap.Control.Mouse(mode);
-
-  map.addControl(mouseControl);
+  const map = useContext<any>(MapContext);
+  const mouseControlRef = useRef(null);
 
   useEffect(() => {
-    return () => { map.removeControl(mouseControl) };
-  })
+    const setup = () => {
+      if (mouseControlRef.current) {
+        map.removeControl(mouseControlRef.current);
+      }
+
+      let mode = 0;
+      if (!disabled) {
+        mode |= pan && window.SMap.MOUSE_PAN;
+        mode |= zoom && window.SMap.MOUSE_ZOOM;
+        mode |= wheel && window.SMap.MOUSE_WHEEL;
+      }
+
+      const newMouseControl = new window.SMap.Control.Mouse(mode);
+
+      map.addControl(newMouseControl);
+      mouseControlRef.current = newMouseControl;
+    };
+
+    setup();
+
+    return () => {
+      map.removeControl(mouseControlRef.current);
+    };
+  });
 
   return null;
-}
+};
 
 MouseControl.defaultProps = {
   pan: false,
   wheel: false,
   zoom: false,
-}
+  disabled: false,
+};
 
-export default MouseControl; 
+export default MouseControl;
