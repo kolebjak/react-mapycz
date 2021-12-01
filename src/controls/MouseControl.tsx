4e93fabd66c9375dc237b424e4743d3b4798d8fd
Michaela Krížová <michaela.krizova@prorocketeers.com>
2021-12-01 10:22:02 +0100

    Reformat MouseControl (#385)

--- src/controls/MouseControl.tsx
+++ src/controls/MouseControl.tsx
@@ -2,52 +2,52 @@ import {useContext, useEffect, useRef} from 'react';
 import {MapContext} from '../Map';
 
 interface MouseControlProps {
-    pan?: boolean;
-    wheel?: boolean;
-    zoom?: boolean;
-    disabled?: boolean;
+  pan?: boolean;
+  wheel?: boolean;
+  zoom?: boolean;
+  disabled?: boolean;
 }
 
 const MouseControl = (props: MouseControlProps) => {
-    const {pan, wheel, zoom, disabled} = props;
+  const {pan, wheel, zoom, disabled} = props;
 
-    const map = useContext<any>(MapContext);
-    const mouseControlRef = useRef(null);
+  const map = useContext<any>(MapContext);
+  const mouseControlRef = useRef(null);
 
-    useEffect(() => {
-        const setup = () => {
-            if (mouseControlRef.current) {
-                map.removeControl(mouseControlRef.current);
-            }
+  useEffect(() => {
+    const setup = () => {
+      if (mouseControlRef.current) {
+        map.removeControl(mouseControlRef.current);
+      }
 
-            let mode = 0;
-            if (!disabled) {
-                mode |= pan && window.SMap.MOUSE_PAN;
-                mode |= zoom && window.SMap.MOUSE_ZOOM;
-                mode |= wheel && window.SMap.MOUSE_WHEEL;
-            }
+      let mode = 0;
+      if (!disabled) {
+        mode |= pan && window.SMap.MOUSE_PAN;
+        mode |= zoom && window.SMap.MOUSE_ZOOM;
+        mode |= wheel && window.SMap.MOUSE_WHEEL;
+      }
 
-            const newMouseControl = new window.SMap.Control.Mouse(mode);
+      const newMouseControl = new window.SMap.Control.Mouse(mode);
 
-            map.addControl(newMouseControl);
-            mouseControlRef.current = newMouseControl;
-        };
+      map.addControl(newMouseControl);
+      mouseControlRef.current = newMouseControl;
+    };
 
-        setup();
+    setup();
 
-        return () => {
-            map.removeControl(mouseControlRef.current);
-        };
-    });
+    return () => {
+      map.removeControl(mouseControlRef.current);
+    };
+  });
 
-    return null;
+  return null;
 };
 
 MouseControl.defaultProps = {
-    pan: false,
-    wheel: false,
-    zoom: false,
-    disabled: false,
+  pan: false,
+  wheel: false,
+  zoom: false,
+  disabled: false,
 };
 
 export default MouseControl;
