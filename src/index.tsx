0265a8641ab7b338246876cfb2a888854cc83729
CrossTheDev <54584277+CrossTheDev@users.noreply.github.com>
2021-12-21 12:18:55 +0100

    Add map handle, add map type (#399)
    Co-authored-by: Michaela Krížová <michaela.krizova@prorocketeers.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 export * from './controls';
-export {default as Map } from './Map';
+
+export {default as Map} from './Map';
 export {default as MarkerLayer} from './MarkerLayer';
 export {default as Marker} from './Marker';
 export {default as BaseLayers} from './BaseLayers';
@@ -8,5 +9,15 @@ export {default as Path} from './Path';
 export {default as PathLayer} from './PathLayer';
 export {default as PathMarker} from './PathMarker';
 export {default as Polygon} from './Polygon';
-export type { MapEvent, Coordinates, MarkerCardConfiguration, GeometryOptions } from './types';
-export { LineStyle } from './types';
+
+export type {
+    MapEvent,
+    SMap,
+    SMapPixel,
+    CardPadding,
+    SMapViewport,
+    Coordinates,
+    MarkerCardConfiguration,
+    GeometryOptions,
+} from './types';
+export {LineStyle} from './types';
